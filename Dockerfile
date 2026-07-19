9ed07ee6ccc858bcebfacabe34985b3e3943e7f4
Oleksandr Dvirko <dvalex02@gmail.com>
2026-07-19 21:25:24 +0300

    solution

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.12.13-slim
+FROM python:3.10.8-slim
 WORKDIR /app
 
 COPY requirements.txt .
--- app/main.py
+++ app/main.py
@@ -1,36 +1,36 @@
 import requests
 import os
 from dotenv import load_dotenv
-import time
 
 
 load_dotenv()
-url = "https://api.weatherapi.com/v1/current.json"
+URL = "https://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
 API_KEY = os.getenv("API_KEY")
 
 params = {
     "key": API_KEY,
-    "q": "Paris",
+    "q": CITY,
     "aqi": "no",
     "lang": "en",
 }
 
+
 def get_weather() -> None:
 
-
-    res = requests.get(url, params=params, timeout=10)
+    res = requests.get(URL, params=params, timeout=10)
     res.raise_for_status()
 
     data = res.json()
 
     location = data["location"]
     current = data["current"]
-    print(f"City: {location['name']}")
-    print(f"Country: {location['country']}")
-    print(f"Temperature: {current['temp_c']} °C")
-    print(f"Condition: {current['condition']['text']}")
-    print(f"Humidity: {current['humidity']}%")
-    print(f"Wind: {current['wind_kph']} km/h")
+    print(f"City: {location["name"]}")
+    print(f"Country: {location["country"]}")
+    print(f"Temperature: {current["temp_c"]} °C")
+    print(f"Condition: {current["condition"]["text"]}")
+    print(f"Humidity: {current["humidity"]}%")
+    print(f"Wind: {current["wind_kph"]} km/h")
 
 
 if __name__ == "__main__":
