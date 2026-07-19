df4fd0a8a76f3e928db90e93bf78dac5b368668e
Oleksandr Dvirko <dvalex02@gmail.com>
2026-07-19 20:49:24 +0300

    made a script to obtain weather in paris

--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+url = "https://api.weatherapi.com/v1/current.json"
+API_KEY = os.getenv("API_KEY")
+
+params = {
+    "key": API_KEY,
+    "q": "Paris",
+    "aqi": "no",
+    "lang": "en",
+}
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    res = requests.get(url, params=params, timeout=10)
+    res.raise_for_status()
+
+    data = res.json()
+
+    location = data["location"]
+    current = data["current"]
+    print(f"City: {location['name']}")
+    print(f"Country: {location['country']}")
+    print(f"Temperature: {current['temp_c']} °C")
+    print(f"Condition: {current['condition']['text']}")
+    print(f"Humidity: {current['humidity']}%")
+    print(f"Wind: {current['wind_kph']} km/h")
 
 
 if __name__ == "__main__":
